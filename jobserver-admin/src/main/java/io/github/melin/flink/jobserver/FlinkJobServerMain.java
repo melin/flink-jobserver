637509288a63b1317f025060c67ebb19ca9a5933
huaixin <libinsong1204@gmail.com>
2022-08-26 16:50:50 +0800

    update

--- jobserver-admin/src/main/java/io/github/melin/flink/jobserver/FlinkJobServerMain.java
+++ jobserver-admin/src/main/java/io/github/melin/flink/jobserver/FlinkJobServerMain.java
@@ -1,4 +1,33 @@
 package io.github.melin.flink.jobserver;
 
-public class FlinkJobServerMain {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * huaixin 2022/3/29 6:44 PM
+ */
+@ImportResource(locations = {"classpath*:jobserver-context.xml"})
+@EnableScheduling
+@EnableAspectJAutoProxy
+@ServletComponentScan
+@EnableTransactionManagement(proxyTargetClass = true)
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
+public class FlinkJobServerMain extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkJobServerMain.class, args);
+    }
 }
